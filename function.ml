b6e6bd1f368e80efc53d450da24cc60da90d4f12
Filippo <admin@caio-corro.fr>
2014-04-30 20:51:27 +0200

    add functions

--- function.ml
+++ function.ml
@@ -1,5 +1,21 @@
 open Complex;;
 
-let z1 = function (c : Complex.t) -> function (x : Complex.t) ->
-    (add (pow x { re = 3.0 ; im = 0.0}) c)
+(* complex sinus *)
+let (sin : Complex.t -> Complex.t) = function v ->
+    { re = sin v.re *. cosh v.im ; im = cos v.re *. sinh v.im}
+;;
+
+(* z^3 + c *)
+let (z1 : Complex.t -> Complex.t -> Complex.t) = function c -> function z ->
+    (add (pow z { re = 3.0 ; im = 0.0}) c)
+;;
+
+(* sin(z) + e^z + c *)
+let (z2 : Complex.t -> Complex.t -> Complex.t) = function c -> function z ->
+    add (add (sin z) (exp z)) c
+;;
+
+(* z^z + z^6 + c *)
+let (z3 : Complex.t -> Complex.t -> Complex.t) = function c -> function z ->
+    add (add (pow z z) (pow z {re = 6.0 ; im = 0.0})) c
 ;;
